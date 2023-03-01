3c4c6da315e6eefed3a5d4c5e30e6683ee564cb2
aghorianducalis <aghorianducalis@gmail.com>
2023-03-01 14:07:15 +0200

    SQL backups updated: parse results added

---
+++ backups/sources.sql
@@ -0,0 +1,4 @@
+insert into MY_TABLE (id, name, created_at, updated_at)
+values  (1, 'chrome bookmarks', '2023-02-25 04:50:11', '2023-02-25 04:50:11'),
+        (2, 'chrome tabs', '2023-02-25 04:50:11', '2023-02-25 04:50:11'),
+        (3, 'android tabs', '2023-02-25 04:50:11', '2023-02-25 04:50:11');
