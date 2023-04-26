f296319f799f820418636ec0ae1b0a869b64a076
aghorianducalis <aghorianducalis@gmail.com>
2023-04-26 17:25:17 +0300

    link parsed from chrome bookmarks; postgresql dump added
    jobs added
    
    job comment added
    
    composer.lock updated
    
    web routes updated
    
    This combined: jobs added + chrome bookmark parser update
    
    Jobs added with example for Domain clean-up.
    It has no logic still; add it in future.
    The amin goal is to use jobs stored in DB.
    
    Parser has been updated too.
    Link DTO was added for transfering parsed results
    to further processing. Just keep it in OOP style.
    
    Link migration has been updated to avoid
    errors caused by varchar length limitation.
    Now the string columns has up to 2000 chars length.
    
    Resolves: #1
    See also: #2

---
+++ app/Jobs/ExtractDomain.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Link;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ExtractDomain implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param Link $link
+     * @return void
+     */
+    public function __construct(public Link $link)
+    {
+        $this->link = $link->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // add a comment
+//        $this->link->domain()->associate(1);
+//        $this->link->save();
+    }
+}
